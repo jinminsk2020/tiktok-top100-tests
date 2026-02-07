ca25589a496f3f826191475d84250fb6c5cf4ae7
jinminsk2020 <61347936+jinminsk2020@users.noreply.github.com>
2026-02-08 02:59:27 +0300

    Update tikcharts.js

--- tikcharts.js
+++ tikcharts.js
@@ -3,12 +3,15 @@ import * as cheerio from "cheerio";
 
 async function testTikCharts() {
   try {
+    console.log("tikcharts.js started");
+
     const url = "https://tikcharts.com/top-songs";
     const { data } = await axios.get(url, {
       headers: { "User-Agent": "Mozilla/5.0" }
     });
 
     console.log("HTML length:", data.length);
+    console.log("HTML preview:", data.slice(0, 500));
 
     const $ = cheerio.load(data);
     const items = $(".song-item");
